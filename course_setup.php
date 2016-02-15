18870629b640a70b2327bf5b6bc45d1a1e000574
Mark Samberg <mjsamber@ncsu.edu>
2016-02-15 14:18:19 -0500

    Bugfix

--- course_setup.php
+++ course_setup.php
@@ -81,8 +81,6 @@ if($action==$saveedits){
 	$id = required_param('id', PARAM_INT);
 	confirm_sesskey();
 	$record = $DB->get_record('local_catalog',array('id'=>$id), '*', MUST_EXIST);
-
-		$record = $DB->get_record('local_catalog_metadata',array('id'=>$id), '*', MUST_EXIST);
 		$editform =  new local_catalog_editcourse(new moodle_url($returnurl, array('action' => $saveedits, 'id'=>$id)), array('record'=>$record));
 		if($formdata = $editform->get_data()){
 			$formdata->description = $formdata->description['text'];
